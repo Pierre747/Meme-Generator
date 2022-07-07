a9d2ae0865ff4bda4a194315f8dc7986367e33bd
Pierre747 <teupon747@hotmail.com>
2022-07-07 11:25:52 +0200

    Feat: CSS animation logo

--- src/components/Meme/Meme.js
+++ src/components/Meme/Meme.js
@@ -1,15 +1,32 @@
 /* eslint-disable quotes */
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, createRef } from "react";
+import { useScreenshot, createFileName } from "use-react-screenshot";
 import "./meme.css";
 
 export default function Meme() {
+  const ref = createRef(null);
+
   const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
   const [allMemes, setAllMemes] = useState([]);
+
+  const [image, takeScreenShot] = useScreenshot({
+    type: "image/jpeg",
+    quality: 1.0,
+  });
+
+  const download = (image, { name = "img", extension = "jpg" } = {}) => {
+    const a = document.createElement("a");
+    a.href = image;
+    a.download = createFileName(extension, name);
+    a.click();
+  };
+
+  const downloadScreenshot = () => takeScreenShot(ref.current).then(download);
 
   useEffect(() => {
     async function getMemes() {
@@ -61,13 +78,18 @@ export default function Meme() {
         </button>
       </div>
       <div className="meme">
-        <img src={meme.randomImage} className="meme__image" alt="meme" />
+        <img
+          ref={ref}
+          src={meme.randomImage}
+          className="meme__image"
+          alt="meme"
+        />
         <h2 className="meme__text top">{meme.topText}</h2>
         <h2 className="meme__text bottom">{meme.bottomText}</h2>
       </div>
-      {/* <button className="save__button" type="button">
-        🌈 Save picture
-      </button> */}
+      <button type="button" onClick={downloadScreenshot}>
+        Download screenshot
+      </button>
     </main>
   );
 }
